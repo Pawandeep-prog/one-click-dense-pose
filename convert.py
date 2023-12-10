e05b733e7e545ea5568a4077383a39221811fdf5
pawandeep <singhpawandeep315@gmail.com>
2023-12-10 12:05:27 +0530

    beautified the code

--- convert.py
+++ convert.py
@@ -5,8 +5,12 @@ import sys
 
 
 parser = ArgumentParser()
-parser.add_argument("--input", type=str, help="Set the input path to the video", required=True)
-parser.add_argument("--out", type=str, help="Set the output path to the video", required=True)
+parser.add_argument(
+    "--input", type=str, help="Set the input path to the video", required=True
+)
+parser.add_argument(
+    "--out", type=str, help="Set the output path to the video", required=True
+)
 args = parser.parse_args()
 
 
@@ -35,15 +39,17 @@ while True:
     ret, frame = cap.read()
     if not ret:
         break
-    
+
     out_frame, out_frame_seg = predictor.predict(frame)
 
     # Write the frame to the output video
     out.write(out_frame_seg)
 
     done += 1
-    percent = int((done/n_frames)*100)
-    sys.stdout.write("\rProgress: [{}{}] {}%".format("="*percent, " "*(100-percent), percent))
+    percent = int((done / n_frames) * 100)
+    sys.stdout.write(
+        "\rProgress: [{}{}] {}%".format("=" * percent, " " * (100 - percent), percent)
+    )
     sys.stdout.flush()
 
 # Release video capture and writer
